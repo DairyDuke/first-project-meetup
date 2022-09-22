214c8f92344d72e56ea2a1b88f0c1313fed3994f
Bradley Lewter <rakugo1@gmail.com>
2022-09-22 15:41:02 -0400

    Add User login backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,12 +1,57 @@
 'use strict';
 const { Model, Validator } = require('sequelize');
+// bcrypt package to compare the password and the hashedPassword
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    // Returns an object with only the User instance
+    // information that is safe to save to a JWT
     toSafeObject() {
       const { id, username, email } = this; // context will be the User instance
       return { id, username, email };
     }
+    // Returns true if matches User instance's hashedPassword
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+    // Uses currentUser scope to return a User with that id
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+    // Searches for one User with the specified credential
+    // (either a USERNAME or EMAIL)
+    // If found, it will validate password by passing it
+    // into the instance's .validatePassword.
+    // If valid, it should return the user by ussing
+    // the currentUser scope.
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+    // Hash the password using bcryptjs' hashSync method.
+    // Create a User with the username, email, and hashedPassword
+    // Returns the created user using the currentUser scope
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+
     static associate(models) {
       // define association here
     }
