ee54c756d7c02f54181a0957d507d2df00113b70
Bradley Lewter <rakugo1@gmail.com>
2022-09-22 15:49:11 -0400

    Add User signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,25 @@
 const express = require('express')
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 const router = express.Router();
+
+// In the route handler, call the signup static method on the User model.
+// If the user is successfully created, then call setTokenCookie and return
+// a JSON response with the user information. If the creation of the user is
+// unsuccessful, then a Sequelize Validation error will be passed onto the
+// next error-handling middleware.
+router.post(
+  '/',
+  async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  }
+);
 
 module.exports = router;
