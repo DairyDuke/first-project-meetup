b04f577a451a96a46e2f13d887e2f278e69b404b
Bradley Lewter <rakugo1@gmail.com>
2022-10-14 15:45:57 -0400

    Signup Form base code added

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -40,6 +40,24 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+// Signup
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+
+
 // REDUCER STUFF
 const initialState = { user: null };
 
