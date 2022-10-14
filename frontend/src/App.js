d0ca843250f650803d0e1c0c1fded94ffc38397a
Bradley Lewter <rakugo1@gmail.com>
2022-10-14 19:40:51 -0400

    Modal base code added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,40 @@
+// import React, { useState, useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { Route, Switch } from 'react-router-dom';
+// import LoginFormPage from './components/LoginFormPage';
+// import SignupFormPage from "./components/SignupFormPage";
+// import * as sessionActions from "./store/session";
+// import Navigation from "./components/Navigation";
+
+// function App() {
+//   const dispatch = useDispatch();
+//   const [isLoaded, setIsLoaded] = useState(false);
+//   useEffect(() => {
+//     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       <Navigation isLoaded={isLoaded} />
+//       {isLoaded && (
+//         <Switch>
+//           <Route path="/login">
+//             <LoginFormPage />
+//           </Route>
+//           <Route path="/signup">
+//             <SignupFormPage />
+//           </Route>
+//         </Switch>
+//       )}
+//     </>
+//   );
+// }
+
+
+// export default App;
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
+import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
@@ -18,9 +51,6 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
@@ -29,6 +59,5 @@ function App() {
     </>
   );
 }
-
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,66 @@
-import React from 'react';
+// import React from 'react';
 
-import './index.css';
+// import './index.css';
 
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App';
 
-import configureStore from './store';
+// import configureStore from './store';
 
-import { restoreCSRF, csrfFetch } from './store/csrf';
+// import { restoreCSRF, csrfFetch } from './store/csrf';
 
-import * as sessionActions from './store/session';
+// import * as sessionActions from './store/session';
+
+// const store = configureStore();
+
+// if (process.env.NODE_ENV !== 'production') {
+//   restoreCSRF();
+
+//   window.csrfFetch = csrfFetch;
+//   window.store = store;
+//   window.sessionActions = sessionActions;
+// }
+
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
+
+// function Root() {
+//   return (
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   );
+// }
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Root />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
+
+
+// TODO: Update Proxy as needed - start backend before testing
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ModalProvider } from "./context/Modal";
+
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -23,16 +68,14 @@ if (process.env.NODE_ENV !== 'production') {
   window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
-
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
@@ -41,8 +84,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
-
-
-// TODO: Update Proxy as needed - start backend before testing
